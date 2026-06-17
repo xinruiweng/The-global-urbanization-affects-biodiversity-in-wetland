6c82345460285b10035d454e950357835a91cac4
Xinrui <xweng3500@gmail.com>
2026-06-17 11:13:30 +0800

    meta-analysis code

--- meta-analysis code/homo_moderators_forest_plot.R
+++ meta-analysis code/homo_moderators_forest_plot.R
@@ -39,29 +39,29 @@ model_homo_reference <- rma.mv(yi, vi,
                             random = ~ 1 | Study_ID/Plot_ID,
                             data = LRR_homo_weighted, 
                             method = "REML")
-#提取mean 95%CI----
+#mean 95%CI----
 ##taxa_grouped----
 LRR_homo_weighted$taxa_grouped <- factor(LRR_homo_weighted$taxa_grouped)
-#构建每个分类的新数据
+
 newdat <- data.frame(
   taxa_grouped = levels(LRR_homo_weighted$taxa_grouped)
 )
-#构建设计矩阵
+
 X <- model.matrix(~ taxa_grouped, data = newdat)
-#计算marginal mean 和95%CI
+#marginal mean 95%CI
 pred <- predict(
   model_homo_biome,
-  newmods = X[, -1],   # 去掉截距列
+  newmods = X[, -1],
   transf = NULL
 )
-#生成dataframe
+#dataframe
 homo_marginal_means <- data.frame(
   taxa_grouped = newdat$taxa_grouped,
   estimate = pred$pred,
   ci_lb = pred$ci.lb,
   ci_ub = pred$ci.ub
 )
-#导出
+#output
 write.csv(
   homo_marginal_means,
   "model_mean_95ci/homo/homoresul_biome.csv",
@@ -70,26 +70,26 @@ write.csv(
 
 ##scale----
 LRR_homo_weighted$scale_grouped <- factor(LRR_homo_weighted$scale_grouped)
-#构建每个分类的新数据
+
 newdat <- data.frame(
   scale_grouped = levels(LRR_homo_weighted$scale_grouped)
 )
-#构建设计矩阵
+
 X <- model.matrix(~ scale_grouped, data = newdat)
-#计算marginal mean 和95%CI
+#marginal mean 95%CI
 pred <- predict(
   model_homo_scale,
-  newmods = X[, -1],   # 去掉截距列
+  newmods = X[, -1],
   transf = NULL
 )
-#生成dataframe
+#dataframe
 homo_marginal_means <- data.frame(
   scale_grouped = newdat$scale_grouped,
   estimate = pred$pred,
   ci_lb = pred$ci.lb,
   ci_ub = pred$ci.ub
 )
-#导出
+#output
 write.csv(
   homo_marginal_means,
   "model_mean_95ci/homo/homoresul_scale.csv",
@@ -99,26 +99,26 @@ write.csv(
 
 ##wetland_type_grouped----
 LRR_homo_weighted$wetland_type_grouped <- factor(LRR_homo_weighted$wetland_type_grouped)
-#构建每个分类的新数据
+
 newdat <- data.frame(
   wetland_type_grouped = levels(LRR_homo_weighted$wetland_type_grouped)
 )
-#构建设计矩阵
+
 X <- model.matrix(~ wetland_type_grouped, data = newdat)
-#计算marginal mean 和95%CI
+#marginal mean 95%CI
 pred <- predict(
   model_homo_wetland_type,
-  newmods = X[, -1],   # 去掉截距列
+  newmods = X[, -1],
   transf = NULL
 )
-#生成dataframe
+#dataframe
 homo_marginal_means <- data.frame(
   wetland_type_grouped = newdat$wetland_type_grouped,
   estimate = pred$pred,
   ci_lb = pred$ci.lb,
   ci_ub = pred$ci.ub
 )
-#导出
+#output
 write.csv(
   homo_marginal_means,
   "model_mean_95ci/homo/homoresul_wetland.csv",
@@ -127,26 +127,26 @@ write.csv(
 
 ##koppen----
 LRR_homo_weighted$koppen_climate <- factor(LRR_homo_weighted$koppen_climate)
-#构建每个分类的新数据
+
 newdat <- data.frame(
   koppen_climate = levels(LRR_homo_weighted$koppen_climate)
 )
-#构建设计矩阵
+
 X <- model.matrix(~ koppen_climate, data = newdat)
-#计算marginal mean 和95%CI
+#marginal mean 95%CI
 pred <- predict(
   model_homo_koppen,
-  newmods = X[, -1],   # 去掉截距列
+  newmods = X[, -1],
   transf = NULL
 )
-#生成dataframe
+#dataframe
 homo_marginal_means <- data.frame(
   koppen_climate = newdat$koppen_climate,
   estimate = pred$pred,
   ci_lb = pred$ci.lb,
   ci_ub = pred$ci.ub
 )
-#导出
+#output
 write.csv(
   homo_marginal_means,
   "model_mean_95ci/homo/supple_use/homoresul_koppen.csv",
@@ -157,67 +157,67 @@ write.csv(
 
 ##income region----
 LRR_homo_weighted$income_region <- factor(LRR_homo_weighted$income_region)
-#构建每个分类的新数据
+
 newdat <- data.frame(
   income_region = levels(LRR_homo_weighted$income_region)
 )
-#构建设计矩阵
+
 X <- model.matrix(~ income_region, data = newdat)
-#计算marginal mean 和95%CI
+#marginal mean 95%CI
 pred <- predict(
   model_homo_income,
-  newmods = X[, -1],   # 去掉截距列
+  newmods = X[, -1],
   transf = NULL
 )
-#生成dataframe
+#dataframe
 homo_marginal_means <- data.frame(
   income_region = newdat$income_region,
   estimate = pred$pred,
   ci_lb = pred$ci.lb,
   ci_ub = pred$ci.ub
 )
-#导出
+#output
 write.csv(
   homo_marginal_means,
-  "model_mean_95ci/homo/supple_use/homoresul_income.csv",
+  "model_mean_95ci/homo/homoresul_income.csv",
   row.names = FALSE
 )
 #reference----
 LRR_homo_weighted$reference_type <- factor(LRR_homo_weighted$reference_type)
-#构建每个分类的新数据
+
 newdat <- data.frame(
   reference_type = levels(LRR_homo_weighted$reference_type)
 )
-#构建设计矩阵
+
 X <- model.matrix(~ reference_type, data = newdat)
-#计算marginal mean 和95%CI
+#marginal mean 95%CI
 pred <- predict(
   model_homo_reference,
-  newmods = X[, -1],   # 去掉截距列
+  newmods = X[, -1],
   transf = NULL
 )
-#生成dataframe
+#dataframe
 homo_marginal_means <- data.frame(
   reference_type = newdat$reference_type,
   estimate = pred$pred,
   ci_lb = pred$ci.lb,
   ci_ub = pred$ci.ub
 )
-#导出
+#output
 write.csv(
   homo_marginal_means,
-  "model_mean_95ci/homo/supple_use/homoresul_reference.csv",
+  "model_mean_95ci/homo/homoresul_reference.csv",
   row.names = FALSE
 )
 
 ##forest plot----
-df <- read.csv("data/homo_mixed_model_mean_95ci.csv")
+df <- read.csv("data/homogeneity_mean_95ci.csv")
 
 df <- df %>%
   mutate(label_n = paste0(lable, " (", n, ")"))
 
 
-# 控制分组顺序
+
 bio_order <- c(
   "Bacteria",
   "Algae",
@@ -269,26 +269,16 @@ df <- df |>
     )
   )
 
-#df <- df |>
-#  dplyr::mutate(
-#    lable = dplyr::case_when(
-#      group == "Biological group" ~ factor(lable, levels = bio_order),
-#      group == "Wetland type"     ~ factor(lable, levels = wetland_order),
-#      group == "Scale"     ~ factor(lable, levels = scale_order),
-#      TRUE                        ~ factor(lable)
-#    )
-#  )
-
-###柱状图拼图----
+##plot----
 x_lim <- range(df$CI_lower, df$CI_upper, na.rm = TRUE)
 
 base_plot <- function(data) {
   data <- data %>%
     mutate(
       color_group = ifelse(
-        CI_lower <= 0 & CI_upper >= 0,  # 误差线跨越0
-        "cross_zero",  # 跨越0的颜色组
-        "not_cross_zero"  # 不跨越0的颜色组
+        CI_lower <= 0 & CI_upper >= 0,
+        "cross_zero",
+        "not_cross_zero"
       )
     )
   
@@ -296,48 +286,48 @@ base_plot <- function(data) {
     x = Estimate,
     y = lable
   )) +
-    # 柱状图 - 根据是否跨越0设置颜色
+    #color
     geom_col(
-      aes(fill = color_group),  # 使用颜色分组
+      aes(fill = color_group),
       width = 0.6,
       alpha = 0.8
     ) +
-    # 误差线 - 根据是否跨越0设置颜色
+    
     geom_errorbarh(
       aes(
         xmin = CI_lower, 
         xmax = CI_upper,
-        color = color_group  # 使用颜色分组
+        color = color_group
       ),
-      height = 0,  # 误差线两端的高度
+      height = 0,
       linewidth = 0.8,
       alpha = 0.7
     ) +
-    # 参考线
+    
     geom_vline(
       xintercept = 0,
       linetype = "dashed",
       color = "grey40",
       linewidth = 1
     ) +
-    # 坐标轴范围
+    
     scale_x_continuous(limits = x_lim) +
-    # 标签
+    
     labs(x = "LRR homogeneity", y = "") +
-    # 颜色方案
+    
     scale_fill_manual(
       values = c(
-        "cross_zero" = "grey50",     # 跨越0：灰色
-        "not_cross_zero" = "#eead0e"  # 不跨越0：#eead0e
+        "cross_zero" = "grey50",
+        "not_cross_zero" = "#eead0e"
       ),
-      guide = "none"  # 不显示图例
+      guide = "none"
     ) +
     scale_color_manual(
       values = c(
-        "cross_zero" = "grey50",     # 跨越0：灰色
-        "not_cross_zero" = "#eead0e"  # 不跨越0：#eead0e
+        "cross_zero" = "grey50",
+        "not_cross_zero" = "#eead0e"
       ),
-      guide = "none"  # 不显示图例
+      guide = "none"
     ) +
     scale_y_discrete(
       labels = function(x) {
@@ -347,7 +337,7 @@ base_plot <- function(data) {
         lab_map[x]
       }
     ) +
-    # 主题
+    
     theme_minimal(base_size = 14) +
     theme(
       legend.position = "none",
@@ -361,7 +351,7 @@ base_plot <- function(data) {
     )
 }
 
-# 创建三个子图
+
 p1 <- base_plot(subset(df, group == "Biological group")) +
   theme(
     axis.title.x = element_blank(),
